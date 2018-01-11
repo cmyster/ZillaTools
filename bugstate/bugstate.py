5feb4d38d85a582a1c964a2298ea3b265387dabe
Amit Ugol <amit.ugol@gmail.com>
2018-01-11 09:42:56 +0200

    Fixed importing.

--- bugstate/bugstate.py
+++ bugstate/bugstate.py
@@ -2,16 +2,14 @@
 This script goes over lists of bugs per predefined bugzilla query and outputs
 a CSV with data to be later digested elsewhere.
 """
-from bugzilla import RHBugzilla
-
-# Local helpers
-import data
 import statistics
 
-BZ = RHBugzilla(data.URL)
+from data import DFGS
+from data import VERSIONS
+
 
 # This first line of output serves as columns titles.
 print('DFG,Version,Total,Filtered,To ON_QA,To VERIFIED,To CLOSE,URL')
-for dfg in data.DFGS:
-    for version in data.VERSIONS:
+for dfg in DFGS:
+    for version in VERSIONS:
         statistics.PrintStatistics(version, dfg)
